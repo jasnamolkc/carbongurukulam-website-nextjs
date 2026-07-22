d827f6f2df6445d146d18fa5848b00e47ec2d901
jasnamolkc <jasnamolkc91@gmail.com>
2026-07-22 14:42:02 +0530

    added new

--- src/app/faculty/page.tsx
+++ src/app/faculty/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
